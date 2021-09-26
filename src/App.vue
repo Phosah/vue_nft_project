1b01dee80e58566aefe062b705188cc17a019c10
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2021-09-26 13:21:58 -0700

    The intial commit of the NFT page

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    
+    <router-view/>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Quattrocento+Sans:wght@400;700&family=Roboto:wght@100;300;400;500;700&display=swap');
+
+*,
+*:before,
+*:after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <div class="fs-1">This is the NFT official page</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
