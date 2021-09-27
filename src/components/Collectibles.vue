3fcfd0e359e977029caf6fde9c78f780755893c2
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2021-09-27 06:55:23 -0700

    update to the sidebar

--- src/components/Collectibles.vue
+++ src/components/Collectibles.vue
@@ -1,50 +1,19 @@
 <template>
     <div>
-<<<<<<< HEAD
-        <div class="my-4">
-
-            <div class="d-flex justify-content-between my-2">
-                <div class="d-flex align-items-center">
-                    <div @click="showMore" class="me-2"><img src="../assets/up-arrow.png" alt="Arrow icon"></div>
-=======
         <div class="my-5">
 
             <div class="d-flex justify-content-between my-2">
                 <div class="d-flex align-items-center">
                     <div @click="showMore" class="me-2 caret" ref="caret"><img src="../assets/up-arrow.png" alt="Arrow icon" ></div>
->>>>>>> 3016eefb64e10b94c16fc75cdc784d314c24caa0
                     <div class="collectibles-title">Collectibles</div>
                 </div>
                 <div><img src="../assets/dots.png" alt="Dot icon"></div>
             </div>
 
-<<<<<<< HEAD
-            <div v-if="showCollectibles">
-                <div v-for="collectible in collectiblesOne" :key="collectible.name" class="collectibles-block d-flex align-items-center rounded-3">
-
-                    <div class="collectibles-item-box mb-3 mx-2">
-                        <div class=""><img class="collectibles-item__photo" :src="collectible.image" alt="Power Stone"></div>
-                        <p class="mb-1 text-center">{{ collectible.name }}</p>
-                        <div class="d-flex justify-content-center">
-                            <div class="collectibles-item__icon-box d-flex align-items-center me-2 px-2 rounded-pill">
-                                <div class="me-1"><img class="collectibles-item__icon" src="../assets/fire.png" alt="Fire photo"></div>
-                                <div class="collectibles-item__text">{{ collectible.firePoints }}</div>
-                            </div>
-                            <div class="d-flex collectibles-item__icon-box align-items-center px-2 rounded-pill">
-                                <div class="me-1"><img class="collectibles-item__icon" src="../assets/lightening.png" alt="Fire photo"></div>
-                                <div class="collectibles-item__text">{{ collectible.lightPoints }}</div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--  -->
-
-                    <div class="collectibles-item-box mb-3 mx-2">
-=======
             <div v-if="showCollectibles" >
                 <div class="collectibles-block d-flex justify-content-between align-items-center flex-wrap rounded-3">
 
                     <div v-for="collectible in collectibles" :key="collectible.name" class="collectibles-item-box mb-3 mx-2">
->>>>>>> 3016eefb64e10b94c16fc75cdc784d314c24caa0
                         <div class=""><img class="collectibles-item__photo" :src="collectible.image" alt="Power Stone"></div>
                         <p class="mb-1 text-center">{{ collectible.name }}</p>
                         <div class="d-flex justify-content-center">
@@ -70,11 +39,7 @@ export default {
     data() {
         return {
             showCollectibles: true,
-<<<<<<< HEAD
-            collectiblesOne: [
-=======
             collectibles: [
->>>>>>> 3016eefb64e10b94c16fc75cdc784d314c24caa0
                 {
                     name: "Power Stone",
                     image: require("../assets/power-stone.png"),
@@ -87,12 +52,7 @@ export default {
                     lightPoints: 22,
                     firePoints: 56
                 },
-<<<<<<< HEAD
-            ],
-            collectiblesTwo: [
-=======
->>>>>>> 3016eefb64e10b94c16fc75cdc784d314c24caa0
-                {
+                 {
                     name: "Moon Tear",
                     image: require("../assets/moon-tear.png"),
                     lightPoints: 16,
@@ -104,18 +64,12 @@ export default {
                     lightPoints: 18,
                     firePoints: 39
                 },
-
-            ],
+            ]
         }
     },
     methods: {
         showMore() {
             this.showCollectibles = !this.showCollectibles;
-<<<<<<< HEAD
-
-=======
-            this.$refs.caret.classList.toggle("rotate");
->>>>>>> 3016eefb64e10b94c16fc75cdc784d314c24caa0
         }
     }
     
@@ -125,7 +79,6 @@ export default {
 <style scoped>
 .collectibles-block {
     background-color: #333333;
-<<<<<<< HEAD
     width: 300px;
     padding: 20px 20px 10px 20px;
 }
@@ -134,25 +87,6 @@ export default {
 }
 .collectibles-item-box {
     width: 43.8%;
-=======
-    width: 100%;
-    padding: 20px;
-    color: #fff;
-}
-.collectibles-title {
-    font-size: 16px;
-    color: #fff;
-}
-.caret{
-    transform: rotate(180deg);
-  cursor: pointer;
-}
-.caret.rotate{
-    transform: rotate(0deg);
-}
-.collectibles-item-box {
-    width: 40%;
->>>>>>> 3016eefb64e10b94c16fc75cdc784d314c24caa0
 }
 .collectibles-item__icon {
     width: 10px;
