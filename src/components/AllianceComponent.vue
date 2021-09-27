225f318bdbe9c4fbdc941ca024b80477e4f5b0fe
Utitofon-Udoekong <mosesrahul37@gmail.com>
2021-09-27 15:59:03 +0100

    Added alliance

--- src/components/AllianceComponent.vue
+++ src/components/AllianceComponent.vue
@@ -1,79 +1,102 @@
 <template>
-<div id="alliance-comp">
-  <img src="@/assets/allianceComponent/caret.png" @click="showAlliance = !showAlliance">
-  <span class="alliance-heading">Alliance</span>
-  <div class="allianceComp" v-if="showAlliance">
-        <div class="mb-3 d-flex justify-content-start">
-            <img class="ms-4" src="@/assets/allianceComponent/group-0.png" />
-            <img class="ms-1" src="@/assets/allianceComponent/emojione_handshake-1.png" />
-            <img class="ms-1" src="@/assets/allianceComponent/group-1.png" />
-        </div>
-        <div v-for="alliance in alliances" v-bind:key="alliance.id" class="d-flex alliance align-items-center justify-content-between">
-            <div class="mx-4">
-                <span>{{ alliance.name }} </span>
+  <div id="alliance-comp">
+    <img
+      src="@/assets/allianceComponent/caret.png"
+      @click="showAllianceMood"
+      ref="caret"
+      class="caret"
+    />
+    <span class="alliance-heading">Alliance</span>
+    <div class="allianceComp" v-if="showAlliance">
+      <div class="mb-3 d-flex justify-content-start">
+        <img class="ms-4" src="@/assets/allianceComponent/group-0.png" />
+        <img
+          class="ms-1"
+          src="@/assets/allianceComponent/emojione_handshake-1.png"
+        />
+        <img class="ms-1" src="@/assets/allianceComponent/group-1.png" />
+      </div>
+      <div
+        v-for="alliance in alliances"
+        v-bind:key="alliance.id"
+        class="d-flex alliance align-items-center justify-content-between"
+      >
+        <div class="mx-4">
+          <span>{{ alliance.name }} </span>
 
-                <img :src="alliance.icon">
-            </div>
-
-            <span class="value">{{ alliance.value }}</span>
+          <img :src="alliance.icon" />
         </div>
-    </div>    
-</div>
+
+        <span class="value">{{ alliance.value }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <script>
-    export default {
-        data() {
-            return {
-            showAlliance: true,
-            alliances: [
-                {
-                id: 1,
-                name: 'Global Ranking',
-                icon: require('@/assets/allianceComponent/vector-1.png'),
-                value: '#23'
-                },
-                {
-                id: 2,
-                name: 'Cities Controlled',
-                icon: require('@/assets/allianceComponent/vector-2.png'),
-                value: '21 Cities'
-                }
-            ]
-            }
+export default {
+  data() {
+    return {
+      showAlliance: true,
+      alliances: [
+        {
+          id: 1,
+          name: "Global Ranking",
+          icon: require("@/assets/allianceComponent/vector-1.png"),
+          value: "#23",
         },
-        methods: {
-        }
-    }
+        {
+          id: 2,
+          name: "Cities Controlled",
+          icon: require("@/assets/allianceComponent/vector-2.png"),
+          value: "21 Cities",
+        },
+      ],
+    };
+  },
+  methods: {
+    showAllianceMood() {
+      this.showAlliance = !this.showAlliance;
+      this.$refs.caret.classList.toggle("rotate");
+    },
+  },
+};
 </script>
 
 <style scoped>
-    @import url('https://fonts.googleapis.com/css2?family=Quattrocento+Sans:wght@700&display=swap');
-    #alliance-comp {
-    color: #fff;
-    }
+@import url("https://fonts.googleapis.com/css2?family=Quattrocento+Sans:wght@700&display=swap");
+#alliance-comp {
+  color: #fff;
+}
 
-    .alliance-heading {
-    font-size: 0.875rem;
-    margin-left: 0.75rem;
-    }
+.alliance-heading {
+  font-size: 0.875rem;
+  margin-left: 0.75rem;
+}
+.caret{
+  transform: rotate(0deg);
+  cursor: pointer;
+}
+.caret.rotate{
+  transform: rotate(180deg);
+}
 
-    .allianceComp {
-    width: 300px;
-    min-height: 142px;
-    padding: 1.25rem 0;
-    background-color: #333333;
-    font-family: 'Quattrocento Sans', sans-serif;
-    border-radius: 6px;
-    }
+.allianceComp {
+  width: 100%;
+  min-height: 142px;
+  padding: 1.25rem 0;
+  background-color: #333333;
+  font-family: "Quattrocento Sans", sans-serif;
+  border-radius: 6px;
+}
 
-    .alliance {
-    margin-bottom: 0.8125rem;
-    }
-    
-    .value {
-        font-size: 0.6875rem;
-        margin-right: 21px;
-    }
+.alliance {
+  margin-bottom: 0.8125rem;
+}
+
+.value {
+  font-size: 0.6875rem;
+  margin-right: 21px;
+}
 </style>
--- src/components/Collectibles.vue
+++ src/components/Collectibles.vue
@@ -37,7 +37,7 @@
 export default {
     data() {
         return {
-            showCollectibles: true,
+            showCollectibles: false,
             collectibles: [
                 {
                     name: "Power Stone",
