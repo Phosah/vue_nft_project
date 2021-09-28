7ef54480e7ee36e1cb671f53618bbc53510a8251
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2021-09-28 01:56:53 -0700

    increase of map height to 95vh

--- src/components/showGeolocation.vue
+++ src/components/showGeolocation.vue
@@ -3,7 +3,7 @@
     :center="center"
     :zoom="7"
     map-type-id="terrain"
-    style="width: 100%; height: 90vh"
+    style="width: 100%; height: 95vh"
   > 
     <GmapMarker
       :key="index"
