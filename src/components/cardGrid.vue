34cec4a10a3bce6e1d0c74c6c3ba97b341752929
Boye <rippyblogger@gmail.com>
2021-09-28 21:18:09 +0400

    collectibles view iteration 0.1

---
+++ src/components/cardGrid.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <b-row>
+      <b-col cols="12" xs="2" md="3" v-for="(item, index) in pool" :key="index">
+        <nft-card />
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import nftCard from "../components/nftCard.vue";
+
+export default {
+  name: "cardGrid",
+  components: {
+    nftCard,
+  },
+  data() {
+    return {
+      pool: [
+        {
+          name: "Power Stone",
+          image: "./images/nftimg.jpeg",
+        },
+        {
+          name: "Power Stone",
+          image: "./images/nftimg.jpeg",
+        },
+        {
+          name: "Power Stone",
+          image: "./images/nftimg.jpeg",
+        },
+        {
+          name: "Power Stone",
+          image: "./images/nftimg.jpeg",
+        },
+        {
+          name: "Power Stone",
+          image: "./images/nftimg.jpeg",
+        },
+        {
+          name: "Power Stone",
+          image: "./images/nftimg.jpeg",
+        },
+        {
+          name: "Power Stone",
+          image: "./images/nftimg.jpeg",
+        },
+        {
+          name: "Power Stone",
+          image: "./images/nftimg.jpeg",
+        },
+      ],
+    };
+    },
+};
+</script>
+
+<style scoped>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
--- src/views/Collectibles.vue
+++ src/views/Collectibles.vue
@@ -1,15 +1,103 @@
 <template>
-    <div>
-        This is the collectibles page
+    <div class="cont">
+        <nav>
+            <navbar/>
+        </nav>
+
+        <b-row class="Collec">
+            <h2 class="m-5">Collectibles</h2>
+        </b-row>
+
+        <b-row align-h="between">
+            <b-col><butttonGroup/></b-col>
+            <b-col class=" end-this v-al mt-3">
+                <span class=" ending army-heading">Recently Added</span>
+                <div>
+                    <img src="../assets/armyComponent/caret.png" @click="showArmyMood" ref="caret" class="caret">
+                </div>
+                
+            </b-col>
+        </b-row>
+
+        <b-row class="m-3">
+            <card-grid/>
+        </b-row>
+
+        <b-row>
+            <b-col md="3" v-for="(item, index) in pool" :key="index">
+                <nft-card/>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
+
 <script>
+import Navbar from '../components/Navbar.vue'
+import NftCard from '../components/nftCard.vue'
+import cardGrid from '../components/cardGrid.vue'
+import butttonGroup from '../components/buttonGroup.vue'
+
 export default {
+    name: "nftCard",
+    data(){
+        return{
+            showArmy: false,
+        }
+    },
+    components:{
+        NftCard,
+        Navbar,
+        butttonGroup,
+        cardGrid
+    },
+    methods:{
+        showArmyMood(){
+        this.showArmy = !this.showArmy
+        this.$refs.caret.classList.toggle("rotate")
+    }
+    }
 
 }
+
 </script>
 
-<style>
+<style scoped>
+
+.cont{
+    background-color: rgba(34, 34, 34, 1);
+}
+
+.v-al{
+    vertical-align: middle;
+}
+
+.end-this{
+    display: flex;
+    justify-content: end;
+    padding-right: 4rem;
+}
+
+.ending{
+    text-align: end;
+    padding-right: 0.5rem;
+}
+
+.Collec{
+    color: white;
+}
+
+span{
+    color: white;
+}
+
+.caret{
+    transform: rotate(0deg);
+    cursor: pointer;
+}
+
+.caret.rotate{
+    transform: rotate(180deg);
+}
 
 </style>
